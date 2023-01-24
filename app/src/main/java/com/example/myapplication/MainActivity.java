3f16269e4a6dbb9e416f409dbd2657062453875c
pepe1973 <bodrogipr@gmail.com>
2023-01-24 09:13:42 +0100

    View binding és lambda

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,57 +4,50 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
+import com.example.myapplication.databinding.ActivityMainBinding;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.button.setOnClickListener(v-> {
                 if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
                     File kulsofajl = new File(getExternalFilesDir(null), "myfile2.txt");
                     try {
                         FileOutputStream fos = new FileOutputStream(kulsofajl);
-                        EditText editText = findViewById(R.id.editText);
-                        fos.write(editText.getText().toString().getBytes());
+                        fos.write(binding.editText.getText().toString().getBytes());
+                        fos.flush();
                         fos.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-            }
         });
 
-        Button button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        binding.button2.setOnClickListener(v-> {
                 if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
                     File kulsofajl = new File(getExternalFilesDir(null), "myfile2.txt");
                     try {
                         FileInputStream fis = new FileInputStream(kulsofajl);
-                        EditText editText = findViewById(R.id.editText);
                         byte[] data = new byte[(int) kulsofajl.length()];
                         fis.read(data);
                         fis.close();
                         String str = new String(data, "UTF-8");
-                        editText.setText(str);
+                        binding.editText.setText(str);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-            }
         });
     }
 }
